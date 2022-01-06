4e830959d4f20be42f4a4ec34028a0447d6b8ab3
goobtoobles <milolfflr@gmail.com>
2022-01-06 13:47:35 -0500

    Add playRound function and checkWin function

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,8 +1,32 @@
 console.log("Hello!");
+const plays = ["Rock", "Paper", "Scissors"];        //create a list of possible plays
 
 //make a function that returns one of "rock", "paper", or "scissors" chosen at random
 function computerPlay() {
-    const plays = ["Rock", "Paper", "Scissors"];        //create a list of possible plays
     let p = Math.floor(Math.random() * plays.length);   //generate a number between 0 and the length of the plays array - 1
     return plays[p];                                    //return a string corresponding to a random play
 }
+
+function playRound(playerSelection, computerSelection) {
+    //return a string declaring win, loss, or tie
+
+    // make the casing of playerSelection comparable to computerSelection's returns
+    playerSelection = playerSelection[0].toUpperCase() + playerSelection.slice(1, playerSelection.length).toLowerCase();
+    if(playerSelection === computerSelection) {  //check for tie
+        return `Tie! You both chose ${playerSelection}.`;
+    } else if(checkWin(playerSelection, computerSelection)){  //check for win
+        return `You win! ${playerSelection} beats ${computerSelection}.`;
+    } else return `You lose! ${computerSelection} beats ${playerSelection}.`;
+}
+
+function checkWin(playerSelection, computerSelection) {
+    // assuming no tie, compare plays and return true in the case of a player win
+    return  (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+            (playerSelection === 'Scissors' && computerSelection === 'Paper');
+    
+}
+
+const playerSelection = 'rock';
+const computerSelection = computerPlay();
+console.log(playRound(playerSelection, computerSelection));
